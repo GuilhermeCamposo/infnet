81e5f5de1c19f5f6fd8c52d70da36b4b5b07f01e
GuilhermeCamposo <guilherme.camposo@me.com>
2013-05-15 14:02:07 -0300

    projeto arvore b

---
+++ ArvoreB/src/arvoreb/ClasseTeste.java
@@ -0,0 +1,11 @@
+package arvoreb;
+
+public class ClasseTeste {
+
+	public static void main(String[] args) {
+		ArvoreB arvore = new ArvoreB(3);
+
+		System.out.println(arvore.buscarValor(7));
+	}
+
+}
